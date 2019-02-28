1fcc0a898f54d55524bd7bd7bfe6f208e5af0f4e
Eddie Schoute <eddieschoute@gmail.com>
2019-02-27 20:22:01 -0500

    Implement an n-controlled toffoli gate

--- circuit_benchmarks/toffoli.py
+++ circuit_benchmarks/toffoli.py
@@ -1,7 +1,7 @@
 import itertools
 import math
 
-from qiskit import QuantumRegister, QuantumCircuit
+from qiskit import QuantumRegister, QuantumCircuit, ClassicalRegister
 from qiskit.circuit import Gate, InstructionSet
 from qiskit.dagcircuit import DAGCircuit
 from qiskit.extensions.standard import *
@@ -10,10 +10,13 @@ from qiskit.qasm import pi
 
 def toffoli(number_qubits: int):
     assert number_qubits >= 2
-    controls = QuantumRegister(number_qubits-1)
-    target = QuantumRegister(1)
-    qc = QuantumCircuit(controls, target, name="toffoli")
-    qc.ncrx(pi/2, target[0], *controls)
+    q = QuantumRegister(number_qubits)
+    qc = QuantumCircuit(q, name="toffoli")
+    # for i in range(number_qubits-1):
+    #     qc.h(controls[i])
+    qc.ntoffoli(q[number_qubits-1], *q[0:number_qubits-1])
+    # qc.measure(controls, c_controls)
+    # qc.measure(target, c_target)
     return qc
 
 class NcrxGate(Gate):
@@ -22,33 +25,32 @@ class NcrxGate(Gate):
     def __init__(self, theta, tgt, *ctls, circ=None):
         """Create new Toffoli gate."""
         assert len(ctls) >= 1
-        super().__init__(f"c^{len(ctls)}rx", [theta], list(ctls) + [tgt], circ)
+        super().__init__(f"c^{len(ctls)}rx", [theta], [tgt] + list(ctls), circ)
 
     def _define_decompositions(self):
         decomposition = DAGCircuit()
-        q = QuantumRegister(3, "q")
-        last_control = q[len(q)-2]
-        target = q[len(q)-1]
+        nr_qubits = len(self.qargs)
+        q = QuantumRegister(nr_qubits)
+        last_control = q[1]
+        target = q[0]
         decomposition.add_qreg(q)
-        decomposition.add_basis_element("cu3", 2, 0, 3)
-        if len(self.qargs) == 2:
-            # Equal to crx
-            crx_theta = Cu3Gate(self.param[0], -pi/2, pi/2, last_control, target)
+        if nr_qubits == 2:
+            # Equal to crx of theta
+            crx_theta = Cu3Gate(self.params[0], -pi/2, pi/2, last_control, target)
             decomposition.apply_operation_back(crx_theta)
         else:
             # Recurse
-            nr_controls = len(q) - 1
-            decomposition.add_basis_element(f"c^{nr_controls-1}rx", nr_controls, 0, 1)
             rule = [
                 # C-sqrt(rx(theta)) gate
-                Cu3Gate(self.param[0]/2, -pi/2, pi/2, last_control, target),
-                NcrxGate(pi/2, q[0:nr_controls-1], last_control), # toffoli
-                Cu3Gate(self.param[0]/2, -pi/2, pi/2, last_control, target).inverse(),
-                NcrxGate(pi/2, q[0:nr_controls-1], last_control), # toffoli
-                NcrxGate(self.param[0]/2, q[0:nr_controls-1], q[nr_controls]) # c^nrx(theta/2) gate on n-1 qubits
+                Cu3Gate(self.params[0]/2, -pi/2, pi/2, last_control, target),
+                NcrxGate(pi, last_control, *q[2:]), # toffoli
+                Cu3Gate(self.params[0]/2, -pi/2, pi/2, last_control, target).inverse(),
+                NcrxGate(pi, last_control, *q[2:]), # toffoli
+                NcrxGate(self.params[0]/2, target, *q[2:]) # c^nrx(theta/2) gate on n-1 qubits
             ]
             for inst in rule:
                 decomposition.apply_operation_back(inst)
+            # decomposition.apply_operation_back(ToffoliGate(q[1], q[2], q[0]))
         self._decompositions = [decomposition]
 
     def inverse(self):
@@ -57,17 +59,16 @@ class NcrxGate(Gate):
 
     def reapply(self, circ):
         """Reapply this gate to corresponding qubits in circ."""
-        self._modifiers(circ.ntoffoli(self.qargs[0:len(self.qargs)-1], self.qargs[len(self.qargs-1)]))
-
+        self._modifiers(circ.ncrx(self.params[0], self.qargs[0], *self.qargs[1:]))
 
 def ncrx(self, theta, tgt, *ctls):
-    """Apply Toffoli to from ctl1 and ctl2 to tgt."""
+    """Apply n-controlled x-rotation(theta) to target from controls"""
     if all(isinstance(ctl, QuantumRegister) for ctl in ctls) and \
             isinstance(tgt, QuantumRegister) and \
             all(len(ctl) == len(tgt) for ctl in ctls):
         instructions = InstructionSet()
         for i in range(ctls[0].size):
-            instructions.add(self.ntoffoli(theta, zip(ctls, itertools.repeat(i)), (tgt, i)))
+            instructions.add(self.ntoffoli(theta, (tgt, i), *zip(ctls, itertools.repeat(i))))
         return instructions
 
     for ctl in ctls:
@@ -76,4 +77,21 @@ def ncrx(self, theta, tgt, *ctls):
     self._check_dups(list(ctls) + [tgt])
     return self._attach(NcrxGate(theta, tgt, *ctls, circ=self))
 
+def ntoffoli(self, tgt, *ctls):
+    """Apply n-controlled Toffoli to tgt with controls."""
+    if all(isinstance(ctl, QuantumRegister) for ctl in ctls) and \
+            isinstance(tgt, QuantumRegister) and \
+            all(len(ctl) == len(tgt) for ctl in ctls):
+        instructions = InstructionSet()
+        for i in range(ctls[0].size):
+            instructions.add(self.ntoffoli((tgt, i), *zip(ctls, itertools.repeat(i))))
+        return instructions
+
+    for ctl in ctls:
+        self._check_qubit(ctl)
+    self._check_qubit(tgt)
+    self._check_dups(list(ctls) + [tgt])
+    return self._attach(NcrxGate(pi, tgt, *ctls, circ=self))
+
 QuantumCircuit.ncrx = ncrx
+QuantumCircuit.ntoffoli = ntoffoli
